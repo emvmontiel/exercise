3717b539b10958b70e7348bcb49a006039b86fa9
emvmontiel <emvmontiel@gmail.com>
2026-07-14 18:00:46 +0800

    renamed user routes

--- user-feature/routes/user.route.js
+++ user-feature/routes/user.route.js
@@ -1,8 +1,8 @@
-const express   = require("express");
-const router    = express.Router();
-
+const express        = require("express");
+const router         = express.Router();
 const userController = require("../controllers/user.controller");
 
-router.post("/register", userController.register);
+router.post("/", userController.registerUser);
+router.get("/", userController.read);
 
 module.exports = router;
