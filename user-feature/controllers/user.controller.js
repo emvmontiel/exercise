36dec6d58fed2e164050b2aa834aa9048b1d7488
emvmontiel <emvmontiel@gmail.com>
2026-07-20 10:08:13 +0800

    added delete user

--- user-feature/controllers/user.controller.js
+++ user-feature/controllers/user.controller.js
@@ -55,9 +55,22 @@ const updateUser = async (req, res) => {
   }
 };
 
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found"});
+    res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'User deletion failed', error: error.message });
+  }
+};
+
 module.exports = { 
   registerUser, 
   getUsers,
   getUserById,
-  updateUser
+  updateUser,
+  deleteUser
 }
--- user-feature/routes/user.route.js
+++ user-feature/routes/user.route.js
@@ -6,5 +6,6 @@ router.post("/", userController.registerUser);
 router.get("/", userController.getUsers);
 router.get("/:id", userController.getUserById);
 router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
